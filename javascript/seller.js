0891f741911733618a8e274b73dfeabc9d8bfd4e
salsabeelomar <omarsalsabeel65@gmail.com>
2022-07-27 11:34:01 +0300

    change the src of the img  relates #3

--- javascript/seller.js
+++ javascript/seller.js
@@ -60,7 +60,7 @@ function diplayLocalItem() {
     let img = document.createElement("img");
     img.setAttribute(
       "src",
-      "https://images.unsplash.com/photo-1604537529428-15bcbeecfe4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
+    element.img
     );
     imgtd.appendChild(img);
 
